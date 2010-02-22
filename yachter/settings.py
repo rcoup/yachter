940b7ceb4a6cc3a1117ab7af4ca42232a3e6fb8e
Robert Coup <robert@coup.net.nz>
2010-02-23 11:30:27 +1300

    clean up settings, add more useful stuff to the sample

--- yachter/settings.py
+++ yachter/settings.py
@@ -1,4 +1,4 @@
-# Django settings for PCC project.
+# Django settings for Yachter project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -40,12 +40,12 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '--fill-this-up-with-some-random-stuff--'
